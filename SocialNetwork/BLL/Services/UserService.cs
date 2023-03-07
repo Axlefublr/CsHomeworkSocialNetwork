4b73bc06848dd46f7c48b27a042e90e221f12e77
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-03-07 22:43:01 +0800

    implement UserService.cs; fix formatting UserRepository.cs

--- SocialNetwork/BLL/Services/UserService.cs
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -1,9 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+
 namespace SocialNetwork.BLL.Services;
 
-public class UserService {
-    
-    public void Register() {
-        
+public class UserService
+{
+
+    IUserRepository userRepository;
+
+    public UserService()
+    {
+        userRepository = new UserRepository();
     }
-    
+
+    public void Register(UserRegistrationData userRegistrationData)
+    {
+        if (string.IsNullOrEmpty(userRegistrationData.FirstName)) throw new ArgumentNullException();
+        if (string.IsNullOrEmpty(userRegistrationData.LastName)) throw new ArgumentNullException();
+        if (string.IsNullOrEmpty(userRegistrationData.Password)) throw new ArgumentNullException();
+        if (string.IsNullOrEmpty(userRegistrationData.Email)) throw new ArgumentNullException();
+
+        if (userRegistrationData.Password.Length < 8) throw new ArgumentNullException();
+        if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email)) throw new ArgumentNullException();
+
+        if (userRepository.FindByEmail(userRegistrationData.Email) != null) throw new ArgumentNullException();
+
+        UserEntity userEntity = new()
+        {
+            firstname = userRegistrationData.FirstName,
+            lastname = userRegistrationData.LastName,
+            password = userRegistrationData.Password,
+            email = userRegistrationData.Email
+        };
+
+        if (userRepository.Create(userEntity) == 0) throw new Exception();
+
+    }
+
 }
