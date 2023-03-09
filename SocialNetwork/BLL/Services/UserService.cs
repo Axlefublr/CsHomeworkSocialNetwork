a897aa01cb16da88371e7aff3a15a7ed50e0e3bd
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-03-09 23:44:09 +0800

    modify UserService.cs

--- SocialNetwork/BLL/Services/UserService.cs
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using SocialNetwork.BLL.Exceptions;
 using SocialNetwork.BLL.Models;
 using SocialNetwork.DAL.Entities;
 using SocialNetwork.DAL.Repositories;
@@ -7,9 +8,7 @@ namespace SocialNetwork.BLL.Services;
 
 public class UserService
 {
-
     IUserRepository userRepository;
-
     public UserService()
     {
         userRepository = new UserRepository();
@@ -17,17 +16,28 @@ public class UserService
 
     public void Register(UserRegistrationData userRegistrationData)
     {
-        if (string.IsNullOrEmpty(userRegistrationData.FirstName)) throw new ArgumentNullException();
-        if (string.IsNullOrEmpty(userRegistrationData.LastName)) throw new ArgumentNullException();
-        if (string.IsNullOrEmpty(userRegistrationData.Password)) throw new ArgumentNullException();
-        if (string.IsNullOrEmpty(userRegistrationData.Email)) throw new ArgumentNullException();
+        if (String.IsNullOrEmpty(userRegistrationData.FirstName))
+            throw new ArgumentNullException();
 
-        if (userRegistrationData.Password.Length < 8) throw new ArgumentNullException();
-        if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email)) throw new ArgumentNullException();
+        if (String.IsNullOrEmpty(userRegistrationData.LastName))
+            throw new ArgumentNullException();
 
-        if (userRepository.FindByEmail(userRegistrationData.Email) != null) throw new ArgumentNullException();
+        if (String.IsNullOrEmpty(userRegistrationData.Password))
+            throw new ArgumentNullException();
 
-        UserEntity userEntity = new()
+        if (String.IsNullOrEmpty(userRegistrationData.Email))
+            throw new ArgumentNullException();
+
+        if (userRegistrationData.Password.Length < 8)
+            throw new ArgumentNullException();
+
+        if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+            throw new ArgumentNullException();
+
+        if (userRepository.FindByEmail(userRegistrationData.Email) != null)
+            throw new ArgumentNullException();
+
+        var userEntity = new UserEntity()
         {
             firstname = userRegistrationData.FirstName,
             lastname = userRegistrationData.LastName,
@@ -35,8 +45,57 @@ public class UserService
             email = userRegistrationData.Email
         };
 
-        if (userRepository.Create(userEntity) == 0) throw new Exception();
+        if (this.userRepository.Create(userEntity) == 0)
+            throw new Exception();
 
     }
 
+    public User Authenticate(UserAuthenticationData userAuthenticationData)
+    {
+        var findUserEntity = userRepository.FindByEmail(userAuthenticationData.Email);
+        if (findUserEntity is null) throw new UserNotFoundException();
+
+        if (findUserEntity.password != userAuthenticationData.Password)
+            throw new WrongPasswordException();
+
+        return ConstructUserModel(findUserEntity);
+    }
+
+    public User FindByEmail(string email)
+    {
+        var findUserEntity = userRepository.FindByEmail(email);
+        if (findUserEntity is null) throw new UserNotFoundException();
+
+        return ConstructUserModel(findUserEntity);
+    }
+
+    public void Update(User user)
+    {
+        var updatableUserEntity = new UserEntity()
+        {
+            id = user.Id,
+            firstname = user.FirstName,
+            lastname = user.LastName,
+            password = user.Password,
+            email = user.Email,
+            photo = user.Photo,
+            favorite_movie = user.FavoriteMovie,
+            favorite_book = user.FavoriteBook
+        };
+
+        if (this.userRepository.Update(updatableUserEntity) == 0)
+            throw new Exception();
+    }
+
+    private User ConstructUserModel(UserEntity userEntity)
+    {
+        return new User(userEntity.id,
+                      userEntity.firstname,
+                      userEntity.lastname,
+                      userEntity.password,
+                      userEntity.email,
+                      userEntity.photo,
+                      userEntity.favorite_movie,
+                      userEntity.favorite_book);
+    }
 }
