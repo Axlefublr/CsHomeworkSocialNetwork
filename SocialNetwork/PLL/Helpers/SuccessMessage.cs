c0c421854ecac56c8d5a87bb28a06419c339e33d
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-03-10 00:27:56 +0800

    add SuccessMessage.cs

---
+++ SocialNetwork/PLL/Helpers/SuccessMessage.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.PLL.Helpers;
+
+public static class SuccessMessage
+{
+    public static void Show(string message)
+    {
+        ConsoleColor originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = originalColor;
+    }
+}
